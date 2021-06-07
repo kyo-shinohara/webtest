0deece4831aa4c36136ecb5c23071ab465263f35
kyo-shinohara <syo07syo@gmail.com>
2021-06-08 00:36:46 +0900

    2

---
+++ js/sample1.js
@@ -0,0 +1,12 @@
+﻿/*更新しないのに最新っぽく見せてる奴*/
+var element = document.getElementById("Dammyright");
+tyr=new Date().getFullYear()
+nn.innerHTML='<p>このWebページの権利は'+tyr+'年時点では基本的にBpptstrapが持ってます。</p>';
+
+/*IE警告*/
+var IEMessage = document.getElementById("ie-message");
+var userAgent = window.navigator.userAgent.toLowerCase();
+if(userAgent.indexOf('msie') != -1 ||
+  userAgent.indexOf('trident') != -1) {
+    IEMessage.style.display = "block";
+}else {}
